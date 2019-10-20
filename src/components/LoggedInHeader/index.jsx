0becd9a5bb6515c394a0fc09ecfd3c93c84a32fa
Tejiri Matthew <tejirimatthew4@gmail.com>
2019-10-20 17:57:04 +0100

    implement switch on logged in page

--- src/components/LoggedInHeader/index.jsx
+++ src/components/LoggedInHeader/index.jsx
@@ -5,38 +5,38 @@ import React from "react";
 import PropTypes from "prop-types";
 
 // styles
-import "./DashboardHeader.scss";
+import "./LoggedInHeader.scss";
 
-const DashboardHeader = ({ showSideNav, handleShowSideNav, pageName }) => {
+const LoggedInHeader = ({ showSideNav, handleShowSideNav, pageName }) => {
   return (
-    <div className="dashboard-header">
-      <div className="dashboard-header__title">{pageName}</div>
-      <div className="dashboard-header__content">
-        <div className="dashboard-header__content__search">
+    <div className="logged-in-header">
+      <div className="logged-in-header__title">{pageName}</div>
+      <div className="logged-in-header__content">
+        <div className="logged-in-header__content__search">
           <img
             src="https://res.cloudinary.com/store-manager/image/upload/v1571510758/barefoot-nomad/magnifying-glass.svg"
             alt="search"
           />
         </div>
-        <div className="dashboard-header__content__notification">
-          <div className="dashboard-header__content__notification-icon">
+        <div className="logged-in-header__content__notification">
+          <div className="logged-in-header__content__notification-icon">
             <img
               src="https://res.cloudinary.com/store-manager/image/upload/v1571531527/barefoot-nomad/notification.svg"
               alt="notification"
             />
           </div>
-          <div className="dashboard-header__content__notification-count">
+          <div className="logged-in-header__content__notification-count">
             <p>12</p>
           </div>
         </div>
-        <div className="dashboard-header__content__profile_photo">
+        <div className="logged-in-header__content__profile_photo">
           <img
             src="https://res.cloudinary.com/store-manager/image/upload/v1571533873/barefoot-nomad/profile.jpg"
             alt="profile"
           />
         </div>
       </div>
-      <div className="dashboard-header__hamburger" onClick={handleShowSideNav}>
+      <div className="logged-in-header__hamburger" onClick={handleShowSideNav}>
         {showSideNav ? (
           <img
             src="https://res.cloudinary.com/store-manager/image/upload/v1571566121/barefoot-nomad/close-side-nav.svg"
@@ -53,10 +53,10 @@ const DashboardHeader = ({ showSideNav, handleShowSideNav, pageName }) => {
   );
 };
 
-DashboardHeader.propTypes = {
+LoggedInHeader.propTypes = {
   showSideNav: PropTypes.bool,
   handleShowSideNav: PropTypes.func,
   pageName: PropTypes.string
 };
 
-export default DashboardHeader;
+export default LoggedInHeader;
