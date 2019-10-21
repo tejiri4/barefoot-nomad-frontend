9ec799a424b06865366c086e66202907b82bf30a
tobslob <kazmobileapp@gmail.com>
2019-10-21 11:11:48 +0100

    Implement loggedIn user dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 
+// styles
+import './Dashboard.scss'
+
+// components
+import DashboardCard from '../../components/DashboardCard';
+
 const Dashboard = () => {
   return (
-    <div>
-      My Dashboard
-      <span>hdhdhdhdhdh</span>
+    <div className="dashboard">
+        <DashboardCard
+          className={"dashboard__pending-trips"}
+          tripNum={2}
+          tripName={"Pending Trips"}
+        />
+        <DashboardCard
+          className={"dashboard__approved-trips"}
+          tripNum={8}
+          tripName={"Approved Trips"}
+        />
+        <DashboardCard
+          className={"dashboard__declined-trips"}
+          tripNum={1}
+          tripName={"Declined Trips"}
+        />
+        <DashboardCard
+          className={"dashboard__total-trips"}
+          tripNum={11}
+          tripName={"Total Trips"}
+        />
     </div>
   );
 };
