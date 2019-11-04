d01c1461f67843e12186fa9478ff6ece9d2ef400
tobslob <kazmobileapp@gmail.com>
2019-11-04 17:20:07 +0100

    Implement chat page

--- src/components/ChatCard/index.js
+++ src/components/ChatCard/index.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import Input from '../Input/index';
+import ChatInfo from '../ChatInfo';
+
+import './ChatCard.scss';
 
 const ChatCard = () => {
     return(
       <div className="chat">
         <div className="chat__header">
           <h3>General Chat</h3>
-          <p>Talk about your travelling experience</p>
+          <p><span>2D</span>Talk about your travelling experience</p>
         </div>
         <div className="chat__section">
-          <div className="chat__room">
-            <img src="" alt="" />
-            <div className="chat__user-details">
-              <h4 className="chat__user-name">Michael Angelo</h4>
-              <p className="chat__user-message">Your return date is too far away... please give some tangible reasons why you would be away for this long.</p>
-            </div>
-          </div>
+          <ChatInfo
+            src="https://res.cloudinary.com/store-manager/image/upload/v1571533873/barefoot-nomad/profile.jpg"
+            name="Michael Angelo"
+            msg="Your return date is too far away... please give some tangible reasons why you would be away for this long."
+          />
+          <ChatInfo
+            src="https://res.cloudinary.com/store-manager/image/upload/v1571533873/barefoot-nomad/profile.jpg"
+            name="Sarah Conor"
+            msg="It's personal"
+          />
           <div className="chat__input-message">
             <Input
               type="textarea"
---
+++ src/pages/Chat/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ChatCard from "../../components/ChatCard";
+
+const Chat = () => {
+  return (
+    <>
+      <ChatCard />
+    </>
+  );
+};
+
+export default Chat;
